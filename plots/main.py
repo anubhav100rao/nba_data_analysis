04b1f6e5e37603fc74a4a1af1784c14c754dec86
anubhav100rao <anubhav100rao@gmail.com>
2023-07-08 00:20:19 +0530

    added matplotlib

--- plots/main.py
+++ plots/main.py
@@ -3,3 +3,7 @@ import numpy as np
 
 
 fig, ax = plt.subplots()
+x = np.linspace(0, 10, 200)  # x data
+y = np.sin(x) + x + 1            # y data
+ax.plot(x, y)                # creates the plot
+fig.show()
